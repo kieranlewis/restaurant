8f999e9b23ea6ece4d406c36243eed793ad91c8a
Kieran Lewis <kieranlewis1334@gmail.com>
2021-09-22 17:06:16 +1200

    Added tabs to switch between content

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ function makeContent() {
     header.textContent = 'Los Pollos Hermanos';
     content.appendChild(header);
 
+    // create tabs
+    createTabs(['Home', 'Menu', 'Contact']);
+
     // create logo
     const logo = new Image();
     logo.src = Logo;
@@ -17,6 +20,20 @@ function makeContent() {
     const blurb = document.createElement('p');
     blurb.textContent = 'The greatest restaurant chain for all you fried chicken needs. We have it all and we pride ourselves from having no links to the illegal drug trade with the great management of our leader Gustavo Fring. Come on down and you will be clucking to all your friends about us.';
     content.appendChild(blurb);
+}
+
+function createTabs(tabs) {
+    for(let i = 0; i < tabs.length; i++) {
+        const button = document.createElement('button');
+        button.textContent = tabs[i];
+        button.setAttribute('id', tabs[i] + '-btn');
+        button.addEventListener('click', switchTab);
+        content.appendChild(button);
+    }
+}
+
+function switchTab() {
+    console.log(this);
 }
 
 makeContent();
