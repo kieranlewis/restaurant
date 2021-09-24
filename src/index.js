c070a781c8a8c4c3c69922c83ffc4b22e13d4dc0
Kieran Lewis <kieranlewis1334@gmail.com>
2021-09-25 09:27:25 +1200

    Moved all style to its own css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { makeContent as homeContent } from './home';
 import { makeContent as menuContent } from './menu';
 import { makeContent as contactContent } from './contact';
-import Logo2 from './images/breaking-bad-los-pollos-hermanos.jpg';
+import './style.css';
 
 const content = document.querySelector('#content');
 const header = document.querySelector('header');
@@ -16,14 +16,6 @@ function createNavBar() {
         button.textContent = tabs[i];
         button.setAttribute('id', tabs[i] + '-btn');
         button.addEventListener('click', switchTab);
-
-        // applying style to buttons
-        button.style.border = 'none';
-        button.style.color = 'white';
-        button.style.backgroundColor = 'blue';
-        button.style.fontSize = '16px';
-        button.style.margin = '8px 0 8px 0';
-        button.style.paddingRight = '40px';
 
         header.appendChild(button);
     }
@@ -63,7 +55,7 @@ function applyStyles() {
 
 function init() {
     createNavBar();
-    applyStyles();
+    //applyStyles();
     homeContent();
 }
 
